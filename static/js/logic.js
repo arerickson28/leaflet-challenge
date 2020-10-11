62a4d852d3ef9f58b7d9decde76d1c7fc443ae98
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-10-11 11:49:31 -0500

    getting started

---
+++ static/js/logic.js
@@ -0,0 +1,23 @@
+// geoJason = https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson
+
+var myMap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// var myMap = L.map("mapid", {
+//     center: [45.52, -122.67],
+//     zoom: 13
+//   });
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(myMap);
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+console.log("hey") ; 
